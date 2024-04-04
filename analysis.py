0c66ad8dbfc60499c14ed0ecf036340d8fd47571
dave <you@example.com>
2024-04-04 16:29:49 +0100

    Load and clean dataset

---
+++ analysis.py
@@ -0,0 +1,7 @@
+# Load Dataset 
+import pandas as pd
+
+iris = pd.read_excel('/Users/davidosullivan/Desktop/pands/pands-project/pands-project/iris_dataset.xlsx')
+
+# Ensure data is loaded correctly
+iris.head()

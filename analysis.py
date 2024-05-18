c4a0fd8580c6133d9eebac77a2ec901d81b1ad39
dave <you@example.com>
2024-05-18 18:18:29 +0100

    Final Commit

--- analysis.py
+++ analysis.py
@@ -93,3 +93,65 @@ plt.show()
 # Create pairplot 
 iris_pairplot = sns.pairplot(iris[['sepal length', 'sepal width','petal length', 'petal width', 'species']], hue='species', height=2)
 plt.show()
+
+# Correlation between Sepal Length & Petal Length 
+# Compute Correlation
+correlation = iris['sepal length'].corr(iris['petal length']).round(2)
+print(f'Correlation Coefficient:{correlation}')
+
+# Visualise Relationship
+plt.figure(figsize=(8,6))
+plt.scatter(iris['sepal length'], iris['petal length'])
+plt.title('Sepal Length Vs Petal Length')
+plt.xlabel('Sepal Length')
+plt.ylabel("Petal Length")
+plt.show()
+
+# Seaborn Scatter Plot w/ Regression Line
+sns.lmplot(x = 'sepal length', y='petal length', data = iris, height = 6)
+plt.title("Scatter plot with regression line of Sepal Length Vs Petal Length")
+plt.show()
+
+# Correlation between Sepal Length & Petal Width 
+# Compute Correlation
+correlation = iris['sepal length'].corr(iris['petal width']).round(2)
+print(f'Correlation Coefficient:{correlation}')
+
+# Visualise Relationship
+plt.figure(figsize=(8,6))
+plt.scatter(iris['sepal length'], iris['petal width'])
+plt.title('Sepal Length Vs Petal Width')
+plt.xlabel('Sepal Length')
+plt.ylabel("Petal Width")
+plt.show()
+
+# Seaborn Scatter Plot w/ Regression Line
+sns.lmplot(x = 'sepal length', y='petal width', data = iris, height = 6)
+plt.title("Scatter plot with regression line of Sepal Length Vs Petal Width")
+plt.show()
+
+# Additional Analysis
+# Plotting the boxplot
+sns.boxplot(x='species', y='sepal length', data=iris, hue = 'species')
+plt.show()
+
+sns.boxplot(x='species', y='sepal width', data=iris, hue = 'species')
+plt.show()
+
+sns.boxplot(x='species', y='petal length', data=iris, hue = 'species')
+plt.show()
+
+sns.boxplot(x='species', y='petal width', data=iris, hue = 'species')
+plt.show()
+
+# Use GroupBy Function to Group different variables together 
+
+mmm_sepal_length = iris.groupby('species')['sepal length'].agg(['min','max','mean']).round(1)
+mmm_sepal_width = iris.groupby('species')['sepal width'].agg(['min','max','mean']).round(1)
+mmm_petal_length = iris.groupby('species')['petal length'].agg(['min','max','mean']).round(1)
+mmm_petal_width = iris.groupby('species')['petal width'].agg(['min','max','mean']).round(1)
+
+print(f'Sepal Length Breakdown: \n {mmm_sepal_length}')
+print(f'Sepal Width Breakdown: \n {mmm_sepal_width}')
+print(f'Petal Length Breakdown: \n {mmm_petal_length}')
+print(f'Petal Width Breakdown: \n {mmm_petal_width}')
